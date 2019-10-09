200f4d33aeee6006a1c6f77cd3394657dd35de94
Dipak Mukesh <DMukesh@spartaglobal.com>
2019-10-09 16:29:06 +0100

    23/27 test passed, 4 test not implemented

--- WeatherAPIProject/OpenWeatherMap_Forecast/Data_Handling/OpenWeatherMapModel.cs
+++ WeatherAPIProject/OpenWeatherMap_Forecast/Data_Handling/OpenWeatherMapModel.cs
@@ -1,8 +1,55 @@
-﻿namespace WeatherAPIProject.OpenWeatherMap_Forecast.Data_Handling
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WeatherAPIProject.OpenWeatherMap_Forecast.Data_Handling
 {
     public class OpenWeatherMapList
     {
         public int dt { get; set; }
+        public Main main { get; set; }
+        public List<Weather> weather { get; set; }
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public Sys sys { get; set; }
+        public string dt_txt { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double pressure { get; set; }
+        public double sea_level { get; set; }
+        public double grnd_level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+    public class Rain
+    {
+        [JsonProperty(PropertyName = "3h")]
+        public double three { get; set; } 
+    }
+    public class Sys
+    {
+        public string pod { get; set; }
     }
     public class OpenWeatherMapCity
     {
@@ -26,7 +73,7 @@
         public double message { get; set; }
         public int cnt { get; set; }
         public OpenWeatherMapCity city { get; set; }
-        public OpenWeatherMapList list { get; set; }
+        public List<OpenWeatherMapList> list { get; set; }
     }
 
 }
--- WeatherAPIProject/Tests/OpenWeatherMapTest.cs
+++ WeatherAPIProject/Tests/OpenWeatherMapTest.cs
@@ -35,42 +35,114 @@ namespace WeatherAPIProject.Tests
             Assert.AreEqual(40, openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.cnt);
         }
 
+        //Test for weather list 
         [Test]
         public void OpenWeatherMapListDT()
+        {
+            //check the Dt length
+            int dt = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].dt.ToString().Length;
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].dt.ToString().Length, dt);
+        }
+
+        [Test]
+        public void OpenWeatherMapListTempCompare()
+        {
+            //compare Temp with Temp Max
+            double temp = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].main.temp;
+            double tempmax = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].main.temp_max;
+            Assert.AreEqual(temp, tempmax);
+        }
+
+        [Test]
+        public void OpenWeatherMapListTemp_Min()
+        {
+        }
+
+        [Test]
+        public void OpenWeatherMapListPressureCompareWithSeaLevel()
+        {
+            //Compare pressure with SeaLevel
+            double pressure = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].main.pressure;
+            double sealevel = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].main.sea_level;
+            Assert.AreEqual(pressure, sealevel);
+        }
+
+        [Test]
+        public void OpenWeatherMapListGrnd_Level()
         {
         }
 
         [Test]
         public void OpenWeatherMapListMainHumditiy()
         {
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].main.humidity.GetType(), typeof(int));
         }
 
         [Test]
-        public void OpenWeatherMapListWeather()
+        public void OpenWeatherMapListTemp_Kf()
         {
         }
 
         [Test]
-        public void OpenWeatherMapListWind()
+        public void OpenWeatherMapListWeatherID()
         {
+            //list for ID rain 
+            var id = new List<int>();
+            id.AddRange(new[] { 500, 501, 502, 503, 504, 511, 520, 521, 522, 531 });
+
+            Assert.Contains(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].weather[0].id, id);
+        }
+
+        [Test]
+        public void OpenWeatherMapListWeatherName()
+        {
+            Assert.AreEqual("Rain", openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].weather[0].main);
+        }
+
+        [Test]
+        public void OpenWeatherMapListCloud()
+        {
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].clouds.all.GetType(), typeof(int));
+        }
+
+        [Test]
+        public void OpenWeatherMapListWindSpeed()
+        {
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].wind.speed.GetType(), typeof(double));
+        }
+
+        [Test]
+        public void OpenWeatherMapListWindDeg()
+        {
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].wind.deg.GetType(), typeof(double));
         }
 
         [Test]
         public void OpenWeatherMapListRain()
         {
+            Assert.GreaterOrEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].rain.three, 0);
         }
 
         [Test]
         public void OpenWeatherMapListSys()
         {
+            Assert.AreEqual(1, openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].sys.pod.Length);
         }
 
         [Test]
         public void OpenWeatherMapListDT_Text()
         {
+            Assert.AreEqual(19, openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.list[0].dt_txt.Length);
         }
 
         //Test for City 
+        [Test]
+        public void OpenWeatherMapCityID()
+        {
+            //testing if the city id matches with api city id 
+            Assert.AreEqual(2643743, openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.id);
+        }
+
         [Test]
         public void OpenWeatherMapCityName()
         {
@@ -80,25 +152,50 @@ namespace WeatherAPIProject.Tests
 
         [Test]
         public void OpenWeatherMapCityTimezone()
-        {
-            Assert.AreEqual("London", openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.timezone);
+        {  
         }
 
         [Test]
         public void OpenWeatherMapCityCoordLat()
         {
-            Assert.Greater(50, openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.coord.lat);
-        }
-
-        //Test for headers
-        [Test]
-        public void HeaderServer()
-        {
+            //test if the latitude type is double
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.coord.lat.GetType(), typeof(double));
         }
 
         [Test]
-        public void HeaderContentLength()
+        public void OpenWeatherMapCityCoordLon()
         {
+            //test if the longitude type is double
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.coord.lat.GetType(), typeof(double));
+        }
+
+        [Test]
+        public void OpenWeatherMapCountryName()
+        {
+            //testing if the city name matches with api country name 
+            Assert.AreEqual("GB", openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.country);
+        }
+
+        [Test]
+        public void OpenWeatherMapCityPopulation()
+        {
+            //test if population is not empty
+            Assert.IsNotNull(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.sunrise);
+        }
+
+        [Test]
+        public void OpenWeatherMapCitySunriseORSunset()
+        {
+            //test if the sunrise/sunset type is int
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.sunrise.GetType(), typeof(int));
+        }
+
+        [Test]
+        public void OpenWeatherMapCitySunriseORSunsetLength()
+        {
+            //test if the sunrise/sunset length 
+            int sun = openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.sunrise.ToString().Length;
+            Assert.AreEqual(openWeatherMapForcast.openWeatherMapDTO.openWeatherMap.city.sunrise.ToString().Length, sun);
         }
     }
 }
